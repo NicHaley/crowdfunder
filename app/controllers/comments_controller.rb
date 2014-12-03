3517062d2d1548facc7c8472c3807b6d3867814c
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-03 17:05:16 -0500

    Set up comments controller and filled out new comment section of show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,32 @@
 class CommentsController < ApplicationController
-  def create
-  end
+	before_filter :load_project
 
-  def show
-  end
+	def show
+		@comment = Comment.find(params[:id])
+	end
 
-  def create
-  end
+	def create
+		@comment = @project.comments.build(comment_params)
+		@comment.user = current_user
 
-  def destroy
-  end
+		if @comment.save
+			redirect_to projects_path, notice: 'Comment created successfully'
+		else
+			render 'project/show'
+		end
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:post, :project_id)
+	end
+
+	def load_project
+		@project = Project.find(params[:project_id])
+	end
 end
