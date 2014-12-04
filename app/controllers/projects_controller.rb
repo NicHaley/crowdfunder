bdc1df2ce53f649938d76f6164ac6eaad390c95a
Chris-Bishop <christopher.allan.bishop@gmail.com>
2014-12-04 13:24:30 -0500

    Wroking with Project edit views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,18 +21,30 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
+
 	end
 
 	def edit
 	end
 
 	def update
+		@project = Project.find(params[:id])
+
+		if @project.update_attributes(project_params)
+			redirect_to project_path(@project)
+		else
+			render :edit
+		end
 	end
+
 
 	def destroy
 	end
 
 	private
+	def set_project
+		@project = Project.find(params[:id])
+	end
 	def project_params
 		params.require(:project).permit(:name, :description, :amount, :tagline, :photo, :end_date, :start_date, rewards_attributes: [:min_amount, :description, :_destroy])
 	end
